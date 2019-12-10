96dab8957bd72ac3ccdf19adb87dd8f0b430c5ff
Ujwal Patil <ujwal@promobitech.com>
2019-12-10 12:56:16 +0530

    Modified handling methods

--- app/jobs/phrasing_base_job.rb
+++ app/jobs/phrasing_base_job.rb
@@ -8,30 +8,38 @@ class PhrasingBaseJob < ApplicationJob
   		puts "command=================#{cmd}, percentage: #{percentage}"
 
   		if cmd.start_with?('phrasing_do_update')
-        updator_status = Phrasing::Updator.new(cmd.split(':').last).update_files
-  			if updator_status == false
-          add_status('phrasing_in_progress_status', 100)
-          add_status('phrasing_in_progress', false)
-          raise StandardError.new("Terminated as no change detected")
-        else
-          add_status('phrasing_in_progress_status', percentage)
-        end
+        handle_file_update(cmd)
   		else
-    		if system(cmd)
-          add_status('phrasing_in_progress_status', percentage)
-    		else
-    			raise StandardError.new("command failed : #{cmd}")
-    		end
-        
-        add_status('phrasing_in_progress', percentage != 100)
+    		handle_system_command(cmd)
   		end
   	end
 
   	yield(command)
   rescue Exception => e
     add_status('phrasing_in_progress', false)
-
+    puts "PhrasingBaseJob :: request_script :: Exception : #{e.message}"
     Rails.logger.error("PhrasingBaseJob :: request_script :: Exception : #{e.message}")
+  end
+
+  def handle_file_update(cmd)
+    updator_status = Phrasing::Updator.new(cmd.split(':').last).update_files
+    if updator_status == false
+      add_status('phrasing_in_progress_status', 100)
+      add_status('phrasing_in_progress', false)
+      raise StandardError.new("Terminated as no change detected")
+    else
+      add_status('phrasing_in_progress_status', percentage)
+    end
+  end
+
+  def handle_system_command(cmd)
+    if system(cmd)
+      add_status('phrasing_in_progress_status', percentage)
+    else
+      raise StandardError.new("command failed : #{cmd}")
+    end
+    
+    add_status('phrasing_in_progress', percentage != 100)
   end
 
   def add_status(key, value)
